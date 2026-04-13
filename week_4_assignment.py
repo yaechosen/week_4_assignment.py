c9f32634141872b974fca0baa989b74f7149eae6
yaechosen <152359243+yaechosen@users.noreply.github.com>
2026-04-13 19:38:18 -0400

    Update week_4_assignment.py

--- week_4_assignment.py
+++ week_4_assignment.py
@@ -1,8 +1,7 @@
 import hashlib
 
-target_hash = "PUT_HASH_HERE"
-
-with open password.txt as file:
+target_hash = "5e737f891db1175442a39fde73e51d781a545506d71c95477a6deb5988bd7f9a"
+with open("password.txt", "r") as file:
     for line in file:
         word = line.strip()
 
